f0ecdd0c641e7cacc974b1b870baec5e1fb299d5
zuemimiag <zuemimiaung.ucsm@gmail.com>
2025-10-14 01:16:12 +0630

    initial commit

---
+++ src/main/java/org/example/emailverification/EmailVerificationApplication.java
@@ -0,0 +1,13 @@
+package org.example.emailverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailVerificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailVerificationApplication.class, args);
+    }
+
+}
